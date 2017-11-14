64b3d28efac9230bc48d855facf7402d6416815c
Aristofanis Chionis <aristofanischionis@gmail.com>
2017-11-14 14:48:01 +0200

    Add files via upload

---
+++ Baseis2/AM_64/src/AM.c
@@ -0,0 +1,99 @@
+#include "AM.h"
+
+int AM_errno = AME_OK;
+
+struct scan_file {
+	int fileDesc;
+	int blocknum;
+	int recordnum;
+};
+
+struct open_file {
+	char *fileName
+	char attrType1;
+	int attrLength1;
+	char attrType2;
+	int attrLength2;
+};
+scan_file* scan_arr;
+open_file* open_arr;
+
+
+void AM_Init() {
+	scan_arr = (scan_file*)calloc(20,sizeof(scan_file));
+	open_arr = (open_file*)calloc(20,sizeof(open_file));
+	return;
+}
+
+
+int AM_CreateIndex(char *fileName,
+	               char attrType1,
+	               int attrLength1,
+	               char attrType2,
+	               int attrLength2) {
+	 int fd;
+   BF_Block *block;
+   char* data;
+   BF_CreateFile(fileName); //create
+   BF_Block_Init(&block);
+   BF_OpenFile(fileName, &fd);
+	 BF_AllocateBlock(fd, block);
+   data = BF_Block_GetData(block);
+	 int root_num = 1;
+   memcpy(data, "AM", 3);
+	 char* new_str = (char*)calloc(19,sizeof(char));
+   sprintf(new_str,";%c;%d;%c;%d;%d;",attrType1,attrLength1,attrType2,attrLength2,root_num);
+   strncat(*data,new_str,strlen(*data)+20);
+   BF_Block_SetDirty(block);
+   BF_UnpinBlock(block);
+   BF_CloseFile(fd);
+   return AME_OK;
+}
+
+
+int AM_DestroyIndex(char *fileName) {
+	int i;
+	for (i = 0; i < 20; i++) {
+		
+	}
+  return AME_OK;
+}
+
+
+int AM_OpenIndex (char *fileName) {
+  return AME_OK;
+}
+
+
+int AM_CloseIndex (int fileDesc) {
+  return AME_OK;
+}
+
+
+int AM_InsertEntry(int fileDesc, void *value1, void *value2) {
+  return AME_OK;
+}
+
+
+int AM_OpenIndexScan(int fileDesc, int op, void *value) {
+  return AME_OK;
+}
+
+
+void *AM_FindNextEntry(int scanDesc) {
+
+}
+
+
+int AM_CloseIndexScan(int scanDesc) {
+  return AME_OK;
+}
+
+
+void AM_PrintError(char *errString) {
+
+}
+
+void AM_Close() {
+
+}
