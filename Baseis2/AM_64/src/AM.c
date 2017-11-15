0eab157272c3863d2873bc7484aeeec8f7ae66e8
Aristofanis Chionis <aristofanischionis@gmail.com>
2017-11-15 17:38:26 +0200

    Add files via upload

--- Baseis2/AM_64/src/AM.c
+++ Baseis2/AM_64/src/AM.c
@@ -6,6 +6,28 @@
 
 int AM_errno = AME_OK;
 
+#define CALL_OR_DIE(call)     \
+  {                           \
+    BF_ErrorCode code = call; \
+    if (code != BF_OK) {      \
+      BF_PrintError(code);    \
+      exit(code);             \
+    }                         \
+	}
+void remove_datadbs(){
+	  int removed;
+	  char filename[] = "EMP-AGE";
+		char filename1[] = "EMP-DNAME";
+		char filename2[] = "EMP-FAULT";
+		char filename3[] = "EMP-NAME";
+		char filename4[] = "EMP-SAL";
+	  removed = remove(filename);
+	  removed = remove(filename1);
+		removed = remove(filename2);
+		removed = remove(filename3);
+		removed = remove(filename4);
+	}
+
 typedef struct {
 	int fileDesc;
 	int blocknum;
@@ -25,9 +47,11 @@ open_file* open_arr;
 
 
 void AM_Init() {
+	remove_datadbs();
+	BF_Init(LRU);
 	scan_arr = (scan_file*)calloc(20,sizeof(scan_file));
 	open_arr = (open_file*)calloc(20,sizeof(open_file));
-	return;
+	//return;
 }
 
 
@@ -39,19 +63,20 @@ int AM_CreateIndex(char *fileName,
 	 int fd;
    BF_Block *block;
    char* data;
-   BF_CreateFile(fileName); //create
+   CALL_OR_DIE(BF_CreateFile(fileName)); //create
    BF_Block_Init(&block);
-   BF_OpenFile(fileName, &fd);
-   BF_AllocateBlock(fd, block);
+   CALL_OR_DIE(BF_OpenFile(fileName, &fd));
+	 printf("%d\n", fd);
+   CALL_OR_DIE(BF_AllocateBlock(fd, block));
    data = BF_Block_GetData(block);
    int root_num = 1;
    memcpy(data, "AM", 3);
-   char* new_str = (char*)calloc(19,sizeof(char));
+   char* new_str = (char*)calloc(21,sizeof(char));
    sprintf(new_str,";%c;%d;%c;%d;%d;",attrType1,attrLength1,attrType2,attrLength2,root_num);
-   strncat(*data,new_str,strlen(*data)+20);
+   strncat(data,new_str,strlen(data)+22);
    BF_Block_SetDirty(block);
-   BF_UnpinBlock(block);
-   BF_CloseFile(fd);
+   CALL_OR_DIE(BF_UnpinBlock(block));
+   BF_CloseFile(fd);  //recheck
    return AME_OK;
 }
 
@@ -59,7 +84,7 @@ int AM_CreateIndex(char *fileName,
 int AM_DestroyIndex(char *fileName) {
 	int i;
 	for (i = 0; i < 20; i++) {
-		
+
 	}
   return AME_OK;
 }
